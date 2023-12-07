e9c594c13f35ae3e94cc93516aff6df44769ed94
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-12-07 11:50:51 -0500

    progress

--- Browser/Browser/View/Nigel.swift
+++ Browser/Browser/View/Nigel.swift
@@ -4,7 +4,8 @@ struct Nigel: View {
     @State private var selectedOptions: Set<String> = []
 
     let options = ["Tabs", "Downloads", "Settings", "Bookmarks", "Favorites", "History", "Actions"]
-    
+    let additionalTexts = ["Option 1", "Option 2", "Option 3"] // Example additional texts
+
     var body: some View {
         NavigationView {
             ZStack {
@@ -17,24 +18,40 @@ struct Nigel: View {
                     // Tabs, Downloads, Settings, etc.
                     VStack(alignment: .leading, spacing: 15) {
                         ForEach(options, id: \.self) { option in
-                            Text(option)
-                                .font(.system(size: 35))
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .scaleEffect(selectedOptions.contains(option) ? 1.5 : 1)
-                                .opacity(selectedOptions.contains(option) ? 0.75 : 0.25)
-                                .padding(.leading, selectedOptions.contains(option) ? 20 : 0)
-                                .padding(.top, selectedOptions.contains(option) ? 10 : 0)
-                                .padding(.bottom, selectedOptions.contains(option) ? 10 : 0)
-                                .onTapGesture {
-                                    withAnimation(.spring()) {
-                                        if selectedOptions.contains(option) {
-                                            selectedOptions.remove(option)
-                                        } else {
-                                            selectedOptions.insert(option)
+                            VStack(alignment: .leading) {
+                                Text(option)
+                                    .font(.system(size: 25))
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .scaleEffect(selectedOptions.contains(option) ? 1.5 : 1)
+                                    .opacity(selectedOptions.contains(option) ? 0.75 : 0.25)
+                                    .padding(.leading, selectedOptions.contains(option) ? 20 : 0)
+                                    .padding(.top, selectedOptions.contains(option) ? 10 : 0)
+                                    .padding(.bottom, selectedOptions.contains(option) ? 10 : 0)
+                                    .onTapGesture {
+                                        withAnimation(.spring()) {
+                                            if selectedOptions.contains(option) {
+                                                selectedOptions.remove(option)
+                                            } else {
+                                                selectedOptions.insert(option)
+                                            }
                                         }
                                     }
+                                HStack(spacing: 20) {
+                                    if selectedOptions.contains(option) {
+                                        HStack {
+                                            ForEach(additionalTexts, id: \.self) { text in
+                                                Text(text)
+                                                    .foregroundColor(.white)
+                                                    .padding()
+                                            }
+                                        }
+                                        .scaleEffect(1.2)
+                                        .transition(.slide)
+                                        .animation(.spring(), value: selectedOptions)
+                                    }
                                 }
+                            }
                         }
                     }
                 }
