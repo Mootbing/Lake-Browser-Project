a89e1937ccf4353e4ea079abd13ed48fe00fc0f6
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-12-07 18:05:33 -0500

    done

--- Browser/Browser/View/New/BrowserBar.swift
+++ Browser/Browser/View/New/BrowserBar.swift
@@ -7,61 +7,68 @@ struct GlassMorphicSearchBar: View {
         ZStack {
             // Background with glass morphism effect
             RoundedRectangle(cornerRadius: 20)
-                .fill(Color.white.opacity(0.25))
-                .background(
-                    VisualEffectView(effect: UIBlurEffect(style: .dark))
-                )
+                .fill(Color.white.opacity(0.05))
+//                .background(
+//                    VisualEffectView(effect: UIBlurEffect(style: .dark))
+//                )
+                .background(Color.white.opacity(0.05))
                 .frame(height: 60) // Adjust to your Figma design height
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.white.opacity(0.08), lineWidth: 1)
+                )
             
             HStack {
                 // Placeholder/Text field
                 HStack {
                     Text(searchText)
-                        .foregroundColor(Color.white.opacity(1))
-                        .padding(.leading, 20) // Adjust to your Figma design
-                        .bold()
-                        .font(.system(size: 14))
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                        .kerning(2)
+                        .foregroundColor(.white)
                     Text(".COM")
-                        .foregroundColor(Color.white.opacity(0.5))
-                        .padding(.leading, -7)
-                        .bold()
                         .font(.system(size: 8))
-                }
-                
-                Spacer() // Pushes all elements to the sides
-                
-                // Search icon
-                Button(action: {
-                    // Define action
-                }) {
-                    Image(systemName: "magnifyingglass")
+                        .kerning(2)
+                        .fontWeight(.bold)
                         .foregroundColor(.white)
-                        .font(.system(size: 12))
+                        .opacity(0.5)
+                        .padding(.leading, -7)
+                        .padding(.top, 4)
                 }
-                .frame(width: 32, height: 32) // Adjust to your Figma design
-                .background(Circle() // This will create the circular shape for the background
-                    .stroke(Color.white.opacity(0.25), lineWidth: 2)) // This adds the border with 0.8 opacity and 1px width
-                .clipShape(Circle())
-                .padding(.trailing, 10) // Adjust to your Figma design
                 
-                // Clear icon
-                Button(action: {
-                    searchText = ""
-                    // Define additional action if needed
-                }) {
-                    Image(systemName: "xmark")
-                        .foregroundColor(.white)
-                        .font(.system(size: 12))
-                }
-                .frame(width: 32, height: 32) // Adjust to your Figma design
-                .background(Color.white.opacity(0.25))
-                .clipShape(Circle())
-                .padding(.trailing, 20) // Adjust to your Figma design
+//                Spacer() // Pushes all elements to the sides
+//                
+//                // Search icon
+//                Button(action: {
+//                    // Define action
+//                }) {
+//                    Image(systemName: "magnifyingglass")
+//                        .foregroundColor(.white)
+//                        .font(.system(size: 12))
+//                }
+//                .frame(width: 32, height: 32) // Adjust to your Figma design
+//                .background(Circle() // This will create the circular shape for the background
+//                    .stroke(Color.white.opacity(0.25), lineWidth: 2)) // This adds the border with 0.8 opacity and 1px width
+//                .clipShape(Circle())
+//                .padding(.trailing, 10) // Adjust to your Figma design
+//                
+//                // Clear icon
+//                Button(action: {
+//                    searchText = ""
+//                    // Define additional action if needed
+//                }) {
+//                    Image(systemName: "xmark")
+//                        .foregroundColor(.white)
+//                        .font(.system(size: 12))
+//                }
+//                .frame(width: 32, height: 32) // Adjust to your Figma design
+//                .background(Color.white.opacity(0.25))
+//                .clipShape(Circle())
+//                .padding(.trailing, 20) // Adjust to your Figma design
             }
             .frame(height: 50) // Adjust to your Figma design height
         }
-        .frame(maxWidth: .infinity)
-        .padding(.horizontal, 16) // Adjust to match the horizontal padding in your Figma design
+        .cornerRadius(20)
     }
 }
 
@@ -80,11 +87,6 @@ struct VisualEffectView: UIViewRepresentable {
 
 struct GlassMorphicSearchBar_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack {
-            GlassMorphicSearchBar()
-                .previewLayout(.sizeThatFits)
-        }
-        .background(Color.black)
-        .ignoresSafeArea(.all)
+        GlassMorphicSearchBar()
     }
 }
--- Browser/Browser/View/Nigel.swift
+++ Browser/Browser/View/Nigel.swift
@@ -18,65 +18,84 @@ struct Nigel: View {
                     .opacity(0.75)
                 
                 // Main content
+            
                 VStack(spacing: 20) {
                     // Tabs, Downloads, Settings, etc.
-                    VStack(alignment: .leading, spacing: 15) {
-                        ForEach(options, id: \.self) { option in
-                            VStack(alignment: .leading) {
-                                Text(option)
-                                    .font(.system(size: 30))
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.white)
-                                    .scaleEffect(selectedOptions.contains(option) ? 1.2 : 1)
-                                    .opacity(selectedOptions.contains(option) ? 0.75 : 0.25)
-                                    .padding(.leading, selectedOptions.contains(option) ? 20 : 0)
-                                    .padding(.top, selectedOptions.contains(option) ? 10 : 0)
-                                    .padding(.bottom, selectedOptions.contains(option) ? 10 : 0)
-                                    .onTapGesture {
-                                        withAnimation(.spring()) {
-                                            if selectedOptions.contains(option) {
-                                                selectedOptions.remove(option)
-                                            } else {
-                                                selectedOptions.insert(option)
-                                            }
-                                        }
-                                    }
-                                HStack(spacing: 20) {
-                                    ScrollView(.horizontal, showsIndicators: false) {
-                                        HStack(spacing: 20) {
-                                            ForEach(additionalTexts, id: \.self) { text in
-                                                VStack() {
-                                                    
-                                                    Text(text)
-                                                        .foregroundColor(.white)
-                                                        .padding(20)
-                                                        .padding(.trailing, 0)
-                                                        .offset(x: selectedOptions.contains(option) ? 0 : 500)
-                                                        .font(.system(size: 12))
+                    ScrollView() {
+                        VStack(alignment: .leading, spacing: 15) {
+                            ForEach(options, id: \.self) { option in
+                                VStack(alignment: .leading) {
+                                    Text(option)
+                                        .font(.system(size: 30))
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.white)
+                                        .scaleEffect(selectedOptions.contains(option) ? 1.2 : 1)
+                                        .opacity(selectedOptions.contains(option) ? 0.75 : 0.25)
+                                        .padding(.leading, selectedOptions.contains(option) ? 20 : 0)
+                                        .padding(.top, selectedOptions.contains(option) ? 10 : 0)
+                                        .padding(.bottom, selectedOptions.contains(option) ? 10 : 0)
+                                        .onTapGesture {
+                                            withAnimation(.spring()) {
+                                                if selectedOptions.contains(option) {
+                                                    selectedOptions.remove(option)
+                                                } else {
+                                                    selectedOptions.insert(option)
                                                 }
                                             }
                                         }
-                                  }
-                                    .background(Color.white.opacity(0.05))
-                                    
-                                    .cornerRadius(20)
-                                    .opacity(selectedOptions.contains(option) ? 1 : -1)
-                                    .padding(.bottom, selectedOptions.contains(option) ? 0 : -60)
-                                    .offset(y: selectedOptions.contains(option) ? 0 : -50)
-                                    .animation(.easeOut(duration: 0.5), value: selectedOptions.contains(option))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 15)
-                                            .stroke(Color.white.opacity(0.08), lineWidth: selectedOptions.contains(option) ? 1 : 0)
-                                    )
+                                    HStack(spacing: 20) {
+                                        ScrollView(.horizontal, showsIndicators: false) {
+                                            HStack(spacing: 20) {
+                                                ForEach(additionalTexts, id: \.self) { text in
+                                                    VStack() {
+                                                        Button(action: {}) {
+                                                            Image(systemName: "xmark")
+                                                                .foregroundColor(.white)
+                                                                .font(.system(size: 12))
+                                                        }
+                                                            .frame(width: 32, height: 32) // Adjust to your Figma design
+                                                            .background(Color.white.opacity(0.05))
+                                                            .clipShape(Circle())
+                                                        
+                                                        Text(text)
+                                                            .foregroundColor(.white)
+                                                            .opacity(0.75)
+                                                            .fontWeight(.semibold)
+                                                            .padding(.top, 3)
+                                                            .kerning(1)
+                                                            .font(.system(size: 12))
+                                                    }
+                                                        .padding(.leading, 30)
+//                                                        .padding(.trailing, 30)
+                                                        .padding(.top, 30)
+                                                        .padding(.bottom, 30)
+//                                                        .offset(x: selectedOptions.contains(option) ? 0 : 0)
+                                                        .opacity(selectedOptions.contains(option) ? 1 : -3)
+//                                                        .offset(y: selectedOptions.contains(option) ? 0 : 20)
+                                                }
+                                            }
+                                        }
+                                        .background(Color.white.opacity(0.05))
+                                        .cornerRadius(20)
+                                        .opacity(selectedOptions.contains(option) ? 1 : -1)
+                                        .padding(.bottom, selectedOptions.contains(option) ? 0 : -150)
+                                        .offset(y: selectedOptions.contains(option) ? 0 : 50)
+                                        .animation(.easeOut(duration: 0.5), value: selectedOptions.contains(option))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .stroke(Color.white.opacity(0.08), lineWidth: selectedOptions.contains(option) ? 1 : 0)
+                                        )
+                                    }
                                 }
                             }
                         }
                     }
+                    .frame(maxHeight: .infinity)
+                    
+                    GlassMorphicSearchBar()
                 }
-                .padding(.leading, 50)
-                .padding(.trailing, 50)
-                
-//                GlassMorphicSearchBar()
+                    .padding(.leading, 35)
+                    .padding(.trailing, 35)
             }
         }
     }
